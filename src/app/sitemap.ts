3404ef87329e71f63e518f978b3f58ed433b28f6
Devendra Dhaked <139852487+dhakeddevendra5@users.noreply.github.com>
2025-08-14 13:39:53 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://devendra.vercel.app";
+  const baseUrl = "https://dhakeddevendra5.vercel.app";
 
   return [
     {
